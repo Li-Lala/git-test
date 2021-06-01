d90e72457af6fc9135edab70c457c535568b7d6d
Li-Lala <2389204791@qq.com>
2021-06-01 13:15:04 +0800

    first Commit

---
+++ src/main/java/com/ljh/Git/gitTEST.java
@@ -0,0 +1,14 @@
+package com.ljh.Git;
+
+/**
+ * @author Li-Lala
+ * @date 2021/5/31 20:55
+ */
+public class gitTEST {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+        System.out.println("hello,Git2");
+
+
+    }
+}

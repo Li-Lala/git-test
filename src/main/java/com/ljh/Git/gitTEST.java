1c33ba3e77fc2457ca5da3bd4b7db6d3689b1664
Li-Lala <2389204791@qq.com>
2021-06-01 13:16:45 +0800

    first Commit

--- src/main/java/com/ljh/Git/gitTEST.java
+++ src/main/java/com/ljh/Git/gitTEST.java
@@ -8,6 +8,7 @@ public class gitTEST {
     public static void main(String[] args) {
         System.out.println("hello,Git");
         System.out.println("hello,Git2");
+        System.out.println("hello,Git3");
 
 
     }
